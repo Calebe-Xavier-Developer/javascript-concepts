c7ffe32243ee37ccf7a9b3c9e1e45f92e343e979
Calebe-Xavier-Developer <57916053+Calebe-Xavier-Developer@users.noreply.github.com>
2021-03-25 23:41:07 -0300

    Tipos de dados (string)

---
+++ Javascript/tipos-dados.js
@@ -0,0 +1,11 @@
+/*
+    * Cadeia de Caracteres
+
+    ""  // aspas duplas
+    ''  // aspas simples
+    ``  // template literals ou template strings (crase inversa) 
+    permite multiplas linhas, permite usar expressao ${}
+
+
+*/
+console.log(` Calebe tem ${20 + 1} anos`);
