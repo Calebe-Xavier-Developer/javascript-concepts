16df92403b25e87ee08a844d3ba357a8fe1d5bbd
Calebe-Xavier-Developer <57916053+Calebe-Xavier-Developer@users.noreply.github.com>
2021-03-25 23:48:13 -0300

    Tipo de dados (Boolean)

--- Javascript/tipos-dados.js
+++ Javascript/tipos-dados.js
@@ -10,3 +10,26 @@
 */
 console.log(` Calebe tem ${20 + 1} anos`);
 
+/* 
+    Number
+        * Números
+
+        22  // inteiros
+        15.9  // reais ou float
+        NaN  // Nota a Number
+        Infinity // infinito
+*/
+
+console.log((45.6 + 54) * 0.5 === Infinity);
+
+
+/* 
+    Boolean
+        * somente 2 valores
+
+            true // verdadeiro
+            false // false
+
+*/
+
+console.log(false == !true);
